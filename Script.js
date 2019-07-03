ce2965d7f7eee98032ab1a0dd795b2f7e2fb1214
Interalive <49893843+Interalive@users.noreply.github.com>
2019-07-03 07:57:38 +0700

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,23 @@
+var app = new Vue({
+  el: "#app",
+  data: {
+    title: "Sport Manager",
+    items: []
+  },
+
+  methods: {
+    addItem: function () {
+      var input = document.getElementById("itemForm");
+
+      if (input.value !== "") {
+        this.items.push({
+          text: input.value
+        })
+        input.value = "";
+      }
+    },
+    deleteItem: function (index) {
+        this.items.splice(index, 1);
+    }
+  }
+});
